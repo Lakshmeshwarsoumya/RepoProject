86c26a4107b58756dba2df83e62c8544c44b79f8
Lakshmeshwarsoumya <lakshmeshwarsoumya1999@gmail.com>
2025-03-10 13:30:03 +0530

    Committed Admin pom pages

--- src/main/java/com/joom/automation/objectrepository/AdminLoginPage.java
+++ src/main/java/com/joom/automation/objectrepository/AdminLoginPage.java
@@ -1,8 +1,35 @@
 package com.joom.automation.objectrepository;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
 
 public class AdminLoginPage {
-	
+	@FindBy(id = "inputEmail")
+	private WebElement adminTextField;
+
+	@FindBy(id = "inputPassword")
+	private WebElement adminPasswordTextField;
+
+	@FindBy(xpath = "//button[text()='Login']")
+	private WebElement loginButton;
+
+	public AdminLoginPage(WebDriver driver) {
+		PageFactory.initElements(driver, this);
+
+	}
+
+	public WebElement getAdminTextField() {
+		return adminTextField;
+	}
+
+	public WebElement getAdminPasswordTextField() {
+		return adminPasswordTextField;
+	}
+
+	public WebElement getLoginButton() {
+		return loginButton;
+	}
 
 }
