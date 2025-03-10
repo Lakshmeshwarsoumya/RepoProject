c43313ba3fd0f598e734feebd04bd81856242747
Lakshmeshwarsoumya <lakshmeshwarsoumya1999@gmail.com>
2025-03-10 14:39:55 +0530

    Committed3rdchanges

--- src/main/java/com/joom/automation/objectrepository/AdminPage.java
+++ src/main/java/com/joom/automation/objectrepository/AdminPage.java
@@ -1,8 +1,28 @@
 package com.joom.automation.objectrepository;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
 
 public class AdminPage {
-	
+	@FindBy(xpath = "(//a[@data-toggle='collapse'])[2]")
+	private WebElement orderMgmtLink;
+
+	@FindBy(xpath = "//a[@href='todays-orders.php']")
+	private WebElement todaysOrdersLink;
+
+	public AdminPage(WebDriver driver) {
+		PageFactory.initElements(driver, this);
+
+	}
+
+	public WebElement getOrderMgmtLink() {
+		return orderMgmtLink;
+	}
+
+	public WebElement getTodaysOrdersLink() {
+		return todaysOrdersLink;
+	}
 
 }
