84ef4d15bd81d92ffa37df13b990acbf0c5ce74c
Lakshmeshwarsoumya <lakshmeshwarsoumya1999@gmail.com>
2025-03-11 11:10:20 +0530

    committed 8

--- src/main/java/com/joom/automation/WebDriverUtility/WebdriverUtility.java
+++ src/main/java/com/joom/automation/WebDriverUtility/WebdriverUtility.java
@@ -3,11 +3,20 @@ package com.joom.automation.WebDriverUtility;
 import java.time.Duration;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class WebdriverUtility {
-	
+
 	public void waitForPageToLoad(WebDriver driver) {
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
-	
+
+	}
+	// Wait until an element is visible
+
+	public void waitForElementPresent(WebDriver driver, WebElement element) {
+		WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(20));
+		wait.until(ExpectedConditions.visibilityOf(element));
 	}
 }
---
+++ src/test/java/com/joom/automation/package1/VerifyTodaysOrder.java
@@ -0,0 +1,28 @@
+package com.joom.automation.package1;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.testng.annotations.Test;
+import org.testng.asserts.SoftAssert;
+
+import com.joom.automation.WebDriverUtility.WebdriverUtility;
+import com.joom.automation.baseutility.BaseClassForAdmin;
+import com.joom.automation.objectrepository.AdminPage;
+
+public class VerifyTodaysOrder extends BaseClassForAdmin {
+	@Test
+	public void todaysOrders() {
+		adp = new AdminPage(driver);
+		adp.todaysOrders();
+
+		WebElement norDisplayed = driver.findElement(By.xpath("(//b[@class='label orange pull-right'])[1]"));
+		WebElement entries = driver.findElement(By.xpath("//div[@class='dataTables_info']"));
+		wlib = new WebdriverUtility();
+		wlib.waitForElementPresent(driver, norDisplayed);
+		SoftAssert sa = new SoftAssert();
+		sa.assertEquals(norDisplayed, true);
+		sa.assertEquals(entries, true);
+
+	}
+
+}
