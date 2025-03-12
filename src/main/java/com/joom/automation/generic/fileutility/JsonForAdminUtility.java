337788e657060c46d4cc019c9456a2daf1226e73
Lakshmeshwarsoumya <lakshmeshwarsoumya1999@gmail.com>
2025-03-12 09:51:23 +0530

    committed 11

--- src/main/java/com/joom/automation/generic/fileutility/JsonForAdminUtility.java
+++ src/main/java/com/joom/automation/generic/fileutility/JsonForAdminUtility.java
@@ -11,7 +11,8 @@ import com.google.protobuf.TextFormat.ParseException;
 public class JsonForAdminUtility {
 	public String readDataFromJson(String key) throws Throwable, IOException, ParseException {
 		JSONParser parse = new JSONParser();
-		Object obj = parse.parse(new FileReader("D:\\HP1\\Desktop\\Workspace\\Workspace\\com.joom.automation\\src\\test\\resources\\configData\\commondataForAdmin.json"));
+		Object obj = parse.parse(new FileReader(
+				"D:\\HP1\\Desktop\\Workspace\\Workspace\\com.joom.automation\\src\\test\\resources\\configData\\commondataForAdmin.json"));
 		// convert java object in to json object using down casting
 		JSONObject map = (JSONObject) obj;
 		String data = (String) map.get(key);
